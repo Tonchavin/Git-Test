832394dd90229b148e4ad8faa61f5281f31d2738
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-06-19 19:42:00 +1000

    add fix test

---
+++ Exercise1/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+
+int a = int.Parse(ReadLine()!);  //int.Parse() работает в себе со строками
+                                //Convert.ToInt32() работает в себе с любыми данными
+WriteLine($"Квадрат числа {a} равен {a * a}");
--- test.md
+++
@@ -1,5 +0,0 @@
-# Hello from first repo
-
-## Hello from local device
-
-## Line for test
